f2aba1d0c5d7992cb5188c57a68f56d285ab4ab3
febianvalentino543-ui <febianvalentino543@gmail.com>
2026-03-03 20:05:21 +0700

    menambahkan implementasi prosedur

--- file1.cpp
+++ file1.cpp
@@ -5,4 +5,12 @@ using namespace std;
 // deklarasi global
 int p, l, luas;
 
+// Implementasi prosedur dan fungsi
+void input() {
+    cout << "Masukkan panjang: ";
+    cin >> p;
+    cout << "Masukkan lebar: ";
+    cin >> l;
+}
+
 
