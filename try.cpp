b9980d2bd884bc453ec88811b9744f55c87bef77
febianvalentino543-ui <febianvalentino543@gmail.com>
2026-03-03 19:52:26 +0700

    menambahkan prosedur

---
+++ try.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+// deklarasi variabel
+float jariJari;
+float phi = 3.14159;
+
+// prosedur untuk menghitung luas lingkaran
+void hitungluas() {
+    cout << "Masukkan jari-jari lingkaran: ";
+    cin >> jariJari;
+}
+
